c839a62e0cdbe6edd7a58cd6992d2b9c6c722a03
David Cleary <davidcgcleary@gmail.com>
2023-01-24 01:17:25 +0000

    Initial files

---
+++ ctest.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+#include "ctest.h"
+#include "one.h"
+
+int main(int argc, char* argv[])
+{
+    cout << "Command line arguments\n";
+    
+    for (int i = 0; i < argc; i ++)
+    {
+        cout << "Argument[" << i << "]: " << argv[i] << "\n";
+    }
+    
+    cout << "Name: " << name << "\n";
+    hello();
+}
---
+++ one.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "one.h"
+
+using namespace std;
+
+void hello(void)
+{
+    cout << "Hello, world!\n";
+}
